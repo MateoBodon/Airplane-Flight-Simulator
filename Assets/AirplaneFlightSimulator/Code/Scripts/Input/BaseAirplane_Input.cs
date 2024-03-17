b619f18f1e9afefa048ddf2430d98e317deab7ec
Mateo Bodon <55267042+MateoBodon@users.noreply.github.com>
2024-03-17 13:09:26 -0400

    Created Input Manager and Editor

--- Assets/AirplaneFlightSimulator/Code/Scripts/Input/BaseAirplane_Input.cs
+++ Assets/AirplaneFlightSimulator/Code/Scripts/Input/BaseAirplane_Input.cs
@@ -6,25 +6,78 @@ using UnityEngine;
 public class BaseAirplane_Input : MonoBehaviour
 {
     #region Variables
-    public float pitch = 0f;
-    public float roll = 0f;
-    public float yaw = 0f;
-    public float throttle = 0f;
-    public int flaps = 3;
-    public float brake = 0f;
+    protected float pitch = 0f;
+    protected float roll = 0f;
+    protected float yaw = 0f;
+    protected float throttle = 0f;
+    public KeyCode brakeKey = KeyCode.Space;
+    protected float brake = 0f;
+    public int maxFlapIncrements = 3;
+    protected int flaps = 0;
+
+    #endregion
+
+    #region Properties
+    public float Pitch
+    {
+        get{return pitch;}
+    }
+    public float Roll
+    {
+        get{return roll;}
+    }
+    public float Yaw
+    {
+        get{return yaw;}
+    }
+    public float Throttle
+    {
+        get{return throttle;}
+    }
+    public int Flaps
+    {
+        get{return flaps;}
+    }
+    public float Brake
+    {
+        get{return brake;}
+    }
     #endregion
 
     #region Builtin Methods
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start(){}
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(brake);
     }
+    #endregion
+
+    #region Custom Methods
+        void HandleInput()
+        {
+            //Process Main Control Input
+            pitch = Input.GetAxis("Vertical");
+            roll = Input.GetAxis("Horizontal");
+            yaw = Input.GetAxis("Yaw");
+            throttle = Input.GetAxis("Throttle");
+
+            //Process Brake Inputs
+            brake = Input.GetKey(brakeKey)? 1f : 0f;
+
+            //Process Flap Inputs
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                flaps += 1;
+            }
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                flaps -= 1;
+            }
+
+            flaps = Mathf.Clamp(flaps, 0, maxFlapIncrements);
+        }
     #endregion
 }
--- Assets/AirplaneFlightSimulator/Code/Scripts/Input/Editor/BaseAirplaneInput_Editor.cs
+++ Assets/AirplaneFlightSimulator/Code/Scripts/Input/Editor/BaseAirplaneInput_Editor.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class BaseAirplaneInput_Editor : MonoBehaviour
+[CustomEditor(typeof(BaseAirplane_Input))]
+public class BaseAirplaneInput_Editor : Editor
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    private BaseAirplane_Input targetInput;
+    #endregion
+
+    #region Builtin Methods
+    void OnEnable()
     {
-        
+        targetInput = (BaseAirplane_Input)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        base.OnInspectorGUI();
+
+        string debugInfo = "";
+        debugInfo += "Pitch: " + targetInput.Pitch + "\n";
+        debugInfo += "Roll: " + targetInput.Roll + "\n";
+        debugInfo += "Yaw: " + targetInput.Yaw + "\n";
+        debugInfo += "Throttle: " + targetInput.Throttle + "\n";
+        debugInfo += "Flaps: " + targetInput.Flaps + "\n";
+        debugInfo += "Brake: " + targetInput.Brake + "\n";
+
+
+        //Custom Editor Code
+        GUILayout.Space(20);
+        EditorGUILayout.TextArea(debugInfo, GUILayout.Height(100));
+        GUILayout.Space(20);
+
+        Repaint();
     }
+    #endregion
 }
