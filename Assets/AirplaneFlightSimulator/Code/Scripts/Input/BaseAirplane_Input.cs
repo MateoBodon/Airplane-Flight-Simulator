dd8d3bc14cf6094efb32433da78b53b5fe6dd3a6
Mateo Bodon <55267042+MateoBodon@users.noreply.github.com>
2024-03-17 12:11:38 -0400

    Initial commit

---
+++ Assets/AirplaneFlightSimulator/Code/Scripts/Input/BaseAirplane_Input.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//add namespace??
+public class BaseAirplane_Input : MonoBehaviour
+{
+    #region Variables
+    public float pitch = 0f;
+    public float roll = 0f;
+    public float yaw = 0f;
+    public float throttle = 0f;
+    public int flaps = 3;
+    public float brake = 0f;
+    #endregion
+
+    #region Builtin Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+}
